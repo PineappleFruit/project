19ec63772a23fce61dc00652fb6e7f5495808c79
bigYellow <1057829499@qq.com>
2018-01-31 16:44:40 +0800

    add upload

--- earth/apply.php
+++ earth/apply.php
@@ -9,8 +9,8 @@
                  <script src="{$config_siteurl}statics/style1/js/carrot.js" type="text/javascript"></script>
 	</head>
 	<body class="bgf2">
-	<template file="Content/header.php"/>
-		<div class="lists-banner">
+		<template file="Content/header.php"/>
+			<div class="lists-banner">
 			<img src="{$config_siteurl}statics/style1/images/lists-banner.jpg" alt="" />
 		</div>
 		<div class="lists-navite bgff fz14 c59">
@@ -27,32 +27,22 @@
 		<div class="lists-content warp clear fz18">
                   <div class="fl email-title">
 				<ul class="fl lists-title bgff">
-					<li class="bg-blue cff bb lits-first">领导信箱</li>
-					<li class="plr10"><a href="{:getCategory(50,'url')}" class="c59">写信需知</a><div class="bb"></div></li>
-					<li class=""><a href="{:U('index/index')}" class="c59">我要写信</a><div class="bb"></div></li>
-                                        <li class=""><a href="{:U('index/index',array('from_type'=>7))}" class="c59">我要办件</a><div class="bb"></div></li>
-                                        <li class=""><a href="{:U('index/problem_list')}" class="c59">网上调查</a><div class="bb"></div></li>
+					<li class="bg-blue cff bb lits-first">办事服务</li>
+                                        <li class="active" ><a href="#" class="c59">在线办件</a><div class="bb"></div></li>
+                                        <li class=""><a href="{:U('index/apply_list')}" class="c59">办件调查</a><div class="bb"></div></li>
 				</ul>
 			</div>
 			<div class="fr bgff lists-info write-info clear">
-				<p class="upload" style="display: none;">{:U('Discuz/index/upload')}</p>
-				<form action="{:U('Discuz/index/msg')}" class="write" method="post" id="submitform" enctype="multipart/form-data" target="form1"> 
-					<p class="fz20 c59 mb10"><b class="fw-b">信访人信息</b>（注意：带<span class="c-red">*</span>号的信息必须填写）</p>
+				<form action="{:U('index/apply')}" id="submitform" class="apply" method="post" enctype="multipart/form-data" target="form1">
+					<p class="fz20 c59 mb10"><b class="fw-b">申请人信息</b>（注意：带<span class="c-red">*</span>号的信息必须填写）</p>
 					<div class="di-vm write-small">
 						<span class="tx-r">姓名：</span>
 						<input type="" name="username" id="" value="" />
 						<i class="c-red">*</i>
 					</div>
 					<div class="di-vm write-small">
-						<span class="tx-r">信件分类：</span>
-						<select name="cid" class="fz18">
-							<option value="1">意见建议</option>
-							<option value="2">投诉举报</option>
-							<option value="3">申诉</option>
-							<option value="4">求决</option>
-							<option value="5">咨询</option>
-							<option value="6">其他</option>
-						</select>
+						<span class="tx-r">工作单位：</span>
+						<input type="" name="work_addr" id="" value="" />
 					</div>
 					<div class="di-vm write-small">
 						<span class="tx-r">证件类型：</span>
@@ -69,12 +59,13 @@
 						<i class="c-red">*</i>
 					</div>
 					<div class="di-vm write-small">
-						<span class="tx-r">联系地址：</span>
-						<input type="" name="address" id="" value="" />
+						<span class="tx-r">联系电话：</span>
+						<input type="" name="phone" id="" value="" />
+						<i class="c-red">*</i>
 					</div>
 					<div class="di-vm write-small">
-						<span class="tx-r">手机号码：</span>
-						<input type="" name="phone" id="" value="" />
+						<span class="tx-r">联系地址：</span>
+						<input type="" name="address" id="" value="" />
 						<i class="c-red">*</i>
 					</div>
 					<div class="di-vm write-small">
@@ -82,41 +73,47 @@
 						<input type="" name="email" id="" value="" />
 						<i class="c-red">*</i>
 					</div>
-					<p class="fz20 c59 mb10 mt60"><b class="fw-b">信访人信息</b>（注意：带<span class="c-red">*</span>号的信息必须填写）</p>
-					<div class="di-vm write-small write-lang">
-						<span class="tx-r">标题：</span>
-						<input type="" name="title" id="" value="" />
-						<i class="c-red">*</i>
-					</div>
+                              
+					<p class="fz20 c59 mb10 mt60"><b class="fw-b">所需信息情况</b>（注意：带<span class="c-red">*</span>号的信息必须填写）</p>
+                                        <div class="di-vm write-small write-lang">
+                                            <span class="tx-r">标题：</span>
+                                            <input type="" name="title" id="" value="" />
+                                            <i class="c-red">*</i>
+                                        </div>
 					<div class="di-vm write-small write-textarea">
-						<span class="tx-r">内容：</span>
+						<span class="tx-r">所需信息的内容描述：</span>
 						<textarea name="content" rows="" cols="" class="fz20"></textarea>
 						<i class="c-red">*</i>
 					</div>
-					<div class="di-vm write-small">
-						<span class="tx-r">相关附件：</span>
-						<input type="file" name="photo" id="" value="" class="upload-input"/>
-					</div>
-					
-					<div class="di-vm write-small write-short">
-						<span class="tx-r">验证码：</span>
-						<input type="" name="code" id="" value="" />
-						<div class="dib all-code">
-							<img  id="verify_img" src="{:U('Discuz/index/msgVerify')}"/>
-						</div>
-						<b class="fz16 write-change">看不清，换一张</b>
+					<div class="di-vm write-small write-textarea">
+						<span class="tx-r">所需信息的用途：</span>
+						<textarea name="description" rows="" cols="" class="fz20"></textarea>
 						<i class="c-red">*</i>
 					</div>
-					
-					<div class="tx-c mt40 fz20 write-btn sure-null">
-						<input class="btn btn-submit mr40 cff bg-blue" type="submit" value="提交" >
+                    <div class="di-vm write-small">
+                        <span class="tx-r">相关附件：</span>
+                        <input type="file" name="photo" id="" value="" class="upload-input"/>
+                    </div>
+                     <input type="hidden" name="type" id="" value="0" />
+                    <div class="di-vm write-small write-short">
+                        <span class="tx-r">验证码：</span>
+                        <input type="" name="code" id="" value="" />
+                        <div class="dib all-code plr10">
+                            <img id="verify_img" src="{:U('index/msgVerify')}"/>
+                        </div>
+                        <b class="fz16 write-change">看不清，换一张</b>
+                        <i class="c-red">*</i>
+                    </div>
+
+					<div class="tx-c mt40 fz20 apply-btn sure-null">
+						<input class="btn btn-submit mr40 cff bg-blue apply-submit" type="submit" value="提交" >
 						<button class="btn c59" type="reset">重置</button>
 					</div>
 				</form>
-				<iframe name="form1"></iframe>
 			</div>
 		</div>
-        <!--弹窗-->
+		<iframe name="form1"></iframe>
+		<!--弹窗-->
 		<div class="dialog-box">
 			<div class="dialog-mask"></div>
 			<div class="dialog-con tx-c">
@@ -128,6 +125,7 @@
 		</div>
 		<!--弹窗结束-->
 	<template file="Content/footer.php"/>
+	</body>
 	</body>
         <script>
             $("#submitform").submit(function(){
--- earth/js/carrot.js
+++ earth/js/carrot.js
@@ -1,6 +1,3 @@
-/**
- * Created by Administrator on 2017/10/20.
- */
 $(function () {
 //	top course
     var $arrowTop = $(".arrowTop");
@@ -47,7 +44,10 @@ $(function () {
         }
         reset();
 //  	console.log(num);
-        $oli[num].className = "active";
+		if($(".course-page li").length > 0){
+			$oli[num].className = "active";
+		}
+        
     }
 	$oli.each(function (index) {
         $(this).click(function () {
@@ -86,55 +86,159 @@ $(function () {
     
     //sdk
     // 百度地图API功能
-		var map = new BMap.Map("allmap");
-		var point = new BMap.Point(116.331398,39.897445);
-		var cityName;
-		map.centerAndZoom(point,12);
-	
-		function myFun(result){
-			cityName = result.name;
-			map.setCenter(cityName);
-//			console.log(cityName)
-			$(".location").text(cityName);
-			$.ajax({
-                    url:"http://api.map.baidu.com/telematics/v3/weather",
-                    type:"get",
-                    data:{
-                          location:cityName,
-                          output:'json',
-                          ak:'6tYzTvGZSOpYB5Oc2YGGOKt8'
-                    },
-                    /*预期服务器端返回的数据类型，假设我现在跨域了，我就改成jsonp 就可以了 */
-                dataType:"jsonp",
-                success:function(data){
-                    $(".data").text(data.date)
-					$(".temp").text(data.results[0].weather_data[0].temperature);
-					
-                }
-            })
-		}
-		var myCity = new BMap.LocalCity();
-		myCity.get(myFun);
-		var list = ["周一","周二","周三","周四","周五","周六","周日"]
-		var today = new Date();
-		var todayNum = today.getUTCDay();
-		$(".data-time").text(list[todayNum - 1]);
-//		console.log(today.getUTCDay())
-
+// 		console.log($("#allmap"))
+    	if($("#allmap").length > 0){
+    		var map = new BMap.Map("allmap");
+			var point = new BMap.Point(116.331398,39.897445);
+			var cityName;
+			map.centerAndZoom(point,12);
+		
+			function myFun(result){
+				cityName = result.name;
+				map.setCenter(cityName);
+	//			console.log(cityName)
+				$(".location").text(cityName);
+				$.ajax({
+	                    url:"http://api.map.baidu.com/telematics/v3/weather",
+	                    type:"get",
+	                    data:{
+	                          location:cityName,
+	                          output:'json',
+	                          ak:'6tYzTvGZSOpYB5Oc2YGGOKt8'
+	                    },
+	                    /*预期服务器端返回的数据类型，假设我现在跨域了，我就改成jsonp 就可以了 */
+	                dataType:"jsonp",
+	                success:function(data){
+	                    $(".data").text(data.date)
+						$(".temp").text(data.results[0].weather_data[0].temperature);
+						
+	                }
+	            })
+			}
+			var myCity = new BMap.LocalCity();
+			myCity.get(myFun);
+			var list = ["周一","周二","周三","周四","周五","周六","周日"]
+			var today = new Date();
+			var todayNum = today.getUTCDay();
+			$(".data-time").text(list[todayNum - 1]);
+    	}
+		
 		
 
-//		$(".header-ul-li li").each(function(index){
-//			console.log($(this).text())
-////			if($(this).text().length > 5){ 
-////				$(this).css("font-size","12px")
-////			}
-//		})
-console.log('xxxx')
-		console.log($(".index-more select>option"))
+
+//		console.log($(".index-more select>option"))
 		$(".index-more select").change(function(){
 //			confirm("你确定提交吗");
 			if (confirm("你确定要离开此页面吗？")) {  
 				window.open($(this).find("option:selected").val());
 	       }  
 		})
+		
+//		console.log($(".info-gover-box div"));
+		var listColor = ["bg-1","bg-2","bg-3","bg-4","bg-5","bg-6","bg-7","bg-8"];
+		$(".info-gover-box div").each(function(index,list){
+			$(this).removeClass("bg-1").addClass(listColor[index]);
+		})
+		
+
+	$(".vote-input li").each(function(index){
+		$(this).click(function(){
+			console.log($(this))
+			$(".vote-input input").removeClass("active")
+			$(".vote-input input").eq(index).addClass("active").siblings().removeClass("active")
+		})
+	})
+	
+	$(".vote-btn .btn-submit").click(function(){
+		var inputId = $("input[name = 'id']").val();
+		var inputChecked = $(".vote-input input.active").attr("name");
+		var url = $(".write-info form").attr("action");
+		var data = {'id':inputId,'name':inputChecked}
+		console.log(data);
+//			$.post(url,data,function(res){
+//				console.log(res);
+//			})
+		$.cookie("nowDate"+inputId,undefined)
+//			console.log($.cookie('nowDate'));
+		if($.cookie('nowDate'+inputId)){
+			alert("今天已投票，请明日再来");
+		}else{
+			$.post(url,data,function(res){
+				console.log(res);
+				if(res == 200){
+					var nowDate = new Date().getTime();
+					$.cookie('nowDate'+inputId, nowDate, { expires: 1 });
+					alert("已成功投票");
+					console.log(nowDate);
+				}
+				if(res == 104){
+					alert("请至少选择一项");
+				}
+			})
+		}
+		
+	})
+	
+	$(".vote-btn button").click(function(){
+		$(".vote-input input").removeClass("active")
+	})
+	
+//	写信表单验证
+	
+//	所有表单共用判断是否为空
+//	$(".sure-null .btn-submit").click(function(){
+//		$(".write-small>input").each(function(index){
+//			if($(this).val() == ''){
+//				$(this).css("border","1px solid red");
+////				alert("请填完所有选项");
+//				return false;
+//			}else{
+//				$(this).css("border","1px solid #dadada");
+//			}
+//		})
+//		$(".write-small>textarea").each(function(){
+//			if($(this).val() == ''){
+//				$(this).css("border","1px solid red");
+////				alert("请填完所有选项");
+//				return false;
+//			}else{
+//				$(this).css("border","1px solid #dadada");
+//			}
+//		})
+//	})
+	
+//	表单验证 邮箱 证件号判断
+	$("form :input").blur(function(){
+		var $parent = $(this).parent();
+		$parent.find(".msg").remove();
+		if($(this).attr("name") == "email"){
+			var emailVal = $.trim($(this).val());
+			var emailRex = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
+			if(!emailRex.test(emailVal)){
+				$(this).css("border","1px solid red");
+				$parent.append("<span class='msg'></span>")
+				return false;
+			}else{
+				$(this).css("border","1px solid #dadada");
+				$parent.find(".msg").remove();
+			}
+		}
+	})
+	
+//	写信验证码切换
+	function changeImg(){ 
+		var imgSrc = $("#verify_img").attr("src");
+		document.getElementById('verify_img').src='http://www.klkfqlr.gov.cn/index.php?g=Discuz&m=index&a=msgVerify'+'&time='+Math.random();void(0);
+	}
+	
+	$(".write-change").click(function(){
+		changeImg();
+	})
+	
+//	$(".write .btn-submit").click(function(){
+//		var num = $("form .msg").length;
+//		if(num){
+//			return false;
+//		}
+//	})
 })
